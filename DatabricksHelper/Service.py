77538e2059921ff97de856cc01eda81831a5c1e8
Bin Yan <bin.y@live.com>
2024-05-26 22:14:10 +0800

    Initial commit

--- DatabricksHelper/Service.py
+++ DatabricksHelper/Service.py
@@ -339,6 +339,10 @@ class Pipeline(LogService):
     self.spark_session.conf.set(key, load_id)
     return load_id
 
+  def save_load_info(self, table, load_id):
+    load_info = {"table":table, "load_id":load_id}
+    self.databricks_dbutils.jobs.taskValues.set(key = "task_load_info", value = json.dumps(load_info))
+
   def get_load_info(self):
     #context = self.databricks_dbutils.notebook.entry_point.getDbutils().notebook().getContext()
     jobGroupId = self.databricks_dbutils.notebook.entry_point.getJobGroupId()
@@ -358,19 +362,19 @@ class Pipeline(LogService):
             StructField("table", StringType(), True),
             StructField("load_id", StringType(), True)
         ])
-        df = self.spark_session.createDataFrame([], schema)
-        df.createOrReplaceTempView('load_info')
+        df_load_info = self.spark_session.createDataFrame([], schema)
+        df_load_info.createOrReplaceTempView('load_info')
 
         for task_key in depend_on_task_keys:
             tasks = [task for task in job.settings.tasks if task.task_key == task_key]
             for task in tasks:
-                load_info_value = self.databricks_dbutils.jobs.taskValues.get(taskKey = task.task_key, key = "load_info", default = "")
+                load_info_value = self.databricks_dbutils.jobs.taskValues.get(taskKey = task.task_key, key = "task_load_info", default = "")
                 print(load_info_value)
                 if load_info_value:
                     load_info = json.loads(load_info_value)
                     all_load_info[load_info["table"]] = load_info
-                    df = df.union(self.spark_session.createDataFrame([(load_info["table"], load_info["load_id"])], schema))
-                    df.createOrReplaceTempView('load_info')
+                    df_load_info = df_load_info.union(self.spark_session.createDataFrame([(load_info["table"], load_info["load_id"])], schema))
+                    df_load_info.createOrReplaceTempView('load_info')
                     #exec(f'{load_info["table"]}_load_id = "{load_info["load_id"]}"')
                     #spark.createDataFrame([(load_info["table"], load_info["load_id"])], ("table", "load_id")).createOrReplaceTempView('load_info')
                     print(f'{load_info["table"]}_load_id')
@@ -378,7 +382,7 @@ class Pipeline(LogService):
                     
         print(all_load_info)
     else:
-        print('No match found')
+        print('No task_run_id found')
     return all_load_info
 
   def clear_table(self, table_names, earlist_time):
